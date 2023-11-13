ead4019385c2672a4eefbc29af9f2f7668bba1bf
theisolatedmonk <96rahulseth@gmail.com>
2023-11-13 17:13:48 +0530

    all element imported

--- app/page.tsx
+++ app/page.tsx
@@ -20,17 +20,19 @@ import illustrationYourUsers from '@/public/images/illustration-your-users.svg';
 import logo from '@/public/images/logo.svg';
 import screenMockups from '@/public/images/screen-mockups.svg';
 
+import { AiFillFacebook, AiFillInstagram, AiFillTwitterSquare } from 'react-icons/ai'
+
 
 export default function Home() {
   return (
-    <div className=" w-full h-screen p-8">
+    <div className=" w-full h-screen  flex flex-col">
       <div className="flex justify-between">
         <div className="flex items-center">
           {/* <Image src={iconMessages} alt={''} /> */}
           {/* <Image src={iconMessages} alt={''} /> */}
           <Image src={logo} alt={''} />
         </div>
-        <Button className={'bg-white text-Pink  hover:text-LightPink rounded-3xl'} ButtonName={'Try It Free'} />
+        <Button className={'bg-white text-Pink  hover:text-LightPink rounded-full py-2 px-8'} ButtonName={'Try It Free'} />
 
       </div>
       <div className="flex flex-col">
@@ -40,7 +42,7 @@ export default function Home() {
         <p className="">
           Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.
         </p>
-        <Button className={'bg-Pink hover:bg-LightPink text-white rounded-3xl w-80'} ButtonName={'Get Started For Free'} />
+        <Button className={'bg-Pink hover:bg-LightPink text-white rounded-full w-80 p-4 '} ButtonName={'Get Started For Free'} />
       </div>
 
       <Image src={screenMockups} alt={''} />
@@ -56,6 +58,56 @@ export default function Home() {
           <h4 className="font-bold text-3xl">1.4k+</h4>
           <h1 className="">Communities Formed</h1>
         </div>
+
+
+      </div>
+      <Section imagePosition={'left'} />
+      <div className="flex flex-col z-40 items-center">
+        <h4 className='font-bold text-3xl'>Ready To Build Your Community?</h4>
+        <Button className={'bg-Pink hover:bg-LightPink text-white rounded-full w-80 p-4 '} ButtonName={'Get Started For Free '} />
+      </div>
+
+
+
+      <div className="flex flex-col">
+        <section className='absolute z-0'>
+          <div className="flex flex-col bg-red-500 ">
+
+
+            <Image src={bgFooterTopDesktop} alt={''} className='w-full  rotate   h-60 mb-10' height={200} width={200} />
+            {/* <Image src={bgFooterTopMobile} alt={''} className='w-full  rotate-180  h-80' height={200} width={200} /> */}
+          </div>
+          {/* image 1 */}
+          {/* image 2 */}
+        </section>
+        <div className="flex w-full z-40 items-center">
+          <div className="flex flex-col">
+            <Image src={logo} alt={''} />
+            <p className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nulla quam, hendrerit lacinia vestibulum a, ultrices quis sem.</p>
+            <div className="flex ">
+              <Image src={iconPhone} alt={''} className='bg-slate-400' />
+              <p className="">Phone: +1-543-123-4567</p>
+            </div>
+            <div className="flex ">
+              <Image src={iconEmail} alt={''} className='bg-slate-400' />
+              <p className="">example@huddle.com</p>
+            </div>
+          </div>
+
+          <div className="flex flex-col">
+            <h3 className="">NEWSLETTER</h3>
+            <p className="">To recieve tips on how to grow your community, sign up to our weekly newsletter. We’ll never send you spam or pass on your email address</p>
+            <div className="flex">
+              <input type="text" className='' />
+              <Button className={'bg-Pink hover:bg-LightPink text-white rounded-md w-40 px-4 py-2'} ButtonName={'Subscribe'} />
+            </div>
+          </div>
+        </div>
+        <div className="flex gap-4 bg-red-600 z-40">
+          <AiFillFacebook className='text-white' />
+          <AiFillInstagram className='text-white' />
+          <AiFillTwitterSquare className='text-white' />
+        </div>
       </div>
     </div>
   )
@@ -67,6 +119,7 @@ export default function Home() {
 
 
 import React from 'react'
+import Section from '@/components/Section';
 
 type Props = {
   className: string
@@ -75,6 +128,6 @@ type Props = {
 
 function Button(props: Props) {
   return (
-    <button className={`p-2  border  px-4 border-Pink   hover:border-LightPink ${props.className}`}>{props.ButtonName}</button>
+    <button className={`  border   border-Pink   hover:border-LightPink ${props.className}`}>{props.ButtonName}</button>
   )
 }
--- components/Section.tsx
+++ components/Section.tsx
@@ -1,6 +1,58 @@
 import clsx from 'clsx';
 import React from 'react'
+import Image from 'next/image';
 import { twMerge } from 'tailwind-merge';
+
+
+import bgFooterTopDesktop from '@/public/images/bg-footer-top-desktop.svg';
+import bgFooterTopMobile from '@/public/images/bg-footer-top-mobile.svg';
+import bgSectionBottomDesktop1 from '@/public/images/bg-section-bottom-desktop-1.svg';
+import bgSectionBottomDesktop2 from '@/public/images/bg-section-bottom-desktop-2.svg';
+import bgSectionBottomMobile1 from '@/public/images/bg-section-bottom-mobile-1.svg'; // Note: * added to the name
+import bgSectionBottomMobile2 from '@/public/images/bg-section-bottom-mobile-2.svg';
+import bgSectionTopDesktop1 from '@/public/images/bg-section-top-desktop-1.svg';
+import bgSectionTopDesktop2 from '@/public/images/bg-section-top-desktop-2.svg';
+import bgSectionTopMobile1 from '@/public/images/bg-section-top-mobile-1.svg';
+import bgSectionTopMobile2 from '@/public/images/bg-section-top-mobile-2.svg';
+import iconCommunities from '@/public/images/icon-communities.svg';
+import iconEmail from '@/public/images/icon-email.svg';
+import iconPhone from '@/public/images/icon-phone.svg';
+import illustrationFlowingConversation from '@/public/images/illustration-flowing-conversation.svg';
+import illustrationGrowTogether from '@/public/images/illustration-grow-together.svg';
+import illustrationYourUsers from '@/public/images/illustration-your-users.svg';
+
+const cardData = [
+    {
+        title: 'Grow Together',
+        paragraph: 'Generate meaningful discussion with your audience and build a strong, loyal community. Think of the insightful coversation you miss out on with a feedback form.',
+        svgImage: illustrationGrowTogether,
+        bgTopMobile: bgSectionTopMobile1,
+        bgTopDesktop: bgSectionTopDesktop1,
+        bgBottomMobile: bgSectionBottomMobile1,
+        bgBottomDesktop: bgSectionBottomDesktop1,
+    },
+    {
+        title: 'Flowing Conversations',
+        paragraph: " You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow.",
+        svgImage: illustrationFlowingConversation,
+        bgTopMobile: '',
+        bgTopDesktop: '',
+        bgBottomMobile: '',
+        bgBottomDesktop: '',
+    },
+    {
+        title: 'Your Users ',
+        paragraph: "It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately. ",
+        svgImage: illustrationYourUsers,
+        bgTopMobile: bgSectionTopMobile2,
+        bgTopDesktop: bgSectionTopDesktop2,
+        bgBottomMobile: bgSectionBottomMobile2,
+        bgBottomDesktop: bgSectionBottomDesktop2,
+    },
+]
+
+
+
 
 type Props = {
 
@@ -10,19 +62,34 @@ type Props = {
 
 export default function Section({ imagePosition }: Props) {
     return (
-        <div className={ }>
-            <section className='absolute'>
-                {/* image 1 */}
-                {/* image 2 */}
-            </section>
-
-            {/* text */}
-            <div className='w-1/2'>
+        <div className='flex flex-col gap-20'>
+            {cardData.map((item, index) => (
+                <div className={'relative w-full bg-yellow-700 flex items-center justify-between '}>
+                    <section className='absolute z-0'>
+                        <div className="flex flex-col bg-red-500 ">
 
 
-            </div>
-            {/* hero image */}
-            <div className='w-1/2'></div>
+                            <Image src={item.bgTopDesktop} alt={''} className='w-full  rotate-180  h-80' height={200} width={200} />
+                            <Image src={item.bgBottomDesktop} alt={''} className='w-full  rotate-180  h-80' height={200} width={200} />
+                        </div>
+                        {/* image 1 */}
+                        {/* image 2 */}
+                    </section>
+
+                    {/* text */}
+                    <div className='w-1/2 z-40 flex flex-col justify-between bg-slate-500 px-44 gap-6'>
+                        <h4 className=" font-bold text-4xl">{item.title}</h4>
+                        <p className="">{item.paragraph}</p>
+
+                    </div>
+                    {/* hero image */}
+                    <div className='w-1/2 z-40 bg-gray-800 flex justify-end px-44'>
+                        <Image src={item.svgImage} alt={''} className='w-full' />
+
+                    </div>
+
+                </div>
+            ))}
 
         </div>
     )
